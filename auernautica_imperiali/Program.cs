72cb858291035419cdf1a42bb4e5b20c9296f0c6
MarclGengsch <magenger@hotmail.com>
2020-10-22 13:55:44 +0200

    test

--- auernautica_imperiali/Program.cs
+++ auernautica_imperiali/Program.cs
@@ -3,7 +3,7 @@
 namespace auernautica_imperiali {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("tobi is toll");
         }
     }
 }
