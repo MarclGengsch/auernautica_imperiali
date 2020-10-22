9e7a222b9006b1cd6a0e1cb2dcb76e6ebb83e87c
user <Gaduso>
2020-10-22 14:18:26 +0200

    Hallo das ist test nr 25

--- auernautica_imperiali/Program.cs
+++ auernautica_imperiali/Program.cs
@@ -3,7 +3,7 @@
 namespace auernautica_imperiali {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("tobi is toll"); //wallah was los
+            Console.WriteLine("tobi is toll"); //wallah was los hi
             Console.WriteLine("tobi is toll");
             Console.WriteLine("Gabsl auch");
         }
