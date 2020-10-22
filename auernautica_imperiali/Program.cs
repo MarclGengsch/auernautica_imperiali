0d4edbdc4857ee70290d010d3487d2a9898eb55f
MarclGengsch <magenger@hotmail.com>
2020-10-22 13:51:14 +0200

    test

--- auernautica_imperiali/Program.cs
+++ auernautica_imperiali/Program.cs
@@ -4,7 +4,6 @@ namespace auernautica_imperiali {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine();
         }
     }
 }
