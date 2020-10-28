0b7c1fa1d27ba3b6603acd60d809b833d2953131
MarclGengsch <magenger@hotmail.com>
2020-10-28 16:47:21 +0100

    28.10.2020

--- auernautica_imperiali/BlueDevil.cs
+++ auernautica_imperiali/BlueDevil.cs
@@ -1,5 +1,6 @@
 ﻿namespace auernautica_imperiali {
     public class BlueDevil : AImperiali {
-        
+        public BlueDevil(int x, int y, int z) : base(x, y, z, 5, 2, 1, 2, 5, 3, 3, 5) {
+        }
     }
 }
--- auernautica_imperiali/GrotBommer.cs
+++ auernautica_imperiali/GrotBommer.cs
@@ -1,5 +1,6 @@
 ﻿namespace auernautica_imperiali {
     public class GrotBommer : AOrk {
-        
+        public GrotBommer(int x, int y, int z) : base(x, y, z, 6, 2, 1, 2, 4, 3, 5, 4) {
+        }
     }
 }
