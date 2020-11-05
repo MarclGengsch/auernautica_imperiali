3d88a66a592b0bb5d5ee5f97d0edd5fc175d44e4
MarclGengsch <magenger@hotmail.com>
2020-11-05 20:21:46 +0100

    fertigstellung

--- auernautica_imperiali/Weapon.cs
+++ auernautica_imperiali/Weapon.cs
@@ -5,6 +5,7 @@ namespace auernautica_imperiali {
         private int _damage;
         private Dictionary<ERange, int> _rangeTable = new Dictionary<ERange, int>();
         private List<EFireArc> _fireDirection = new List<EFireArc>();
+        
 
         public Weapon(List<EFireArc> fireDirections, List<int> fireRanges, int damage) {
             _fireDirection = fireDirections;
