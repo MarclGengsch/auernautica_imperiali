872cc13ce932a5b253899898d746790ae95fe38e
MarclGengsch <magenger@hotmail.com>
2020-11-05 20:22:14 +0100

    fertigstellung

---
+++ auernautica_imperiali.unittest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace auernautica_imperiali.unittest {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+            Assert.Pass();
+        }
+    }
+}
