d1111b0942afa8eff6e5de1c3055213f30139b1c
Gaduso <g4duso@gmail.com>
2020-11-05 21:55:15 +0100

    1.11.2020 Latest

--- auernautica_imperiali.unittest/UnitTest1.cs
+++ auernautica_imperiali.unittest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace auernautica_imperiali.unittest {
@@ -7,12 +9,25 @@ namespace auernautica_imperiali.unittest {
         }
 
         [Test]
-        public void Test1() {
+        public void Point() {
             Point p1 = new Point(1, 2, 3);
             Point p2 = new Point(1, 2, 3);
-            Point p3 = new Point(1, 3, 3);
+            Point p3 = new Point(2, 3, 3);
+            Point p4 = new Point(15, 3, 3);
+            AirCraftFactory.GetInstance().MakeAircraft(1, 2, 3, EAirCraftType.EXECUTIONER);
             Assert.True(p1.Equals(p2));
-            Assert.False(p1.Equals(p3));
+            //Assert.False(p1.Equals(p3));
+            Assert.AreEqual("X: 1, Y: 2, Z: 3", p1.ToString());
+            Assert.False(p1.IsPointFree());
+            //Assert.True(p3.IsPointFree());
+            Assert.False(p4.IsPointLegal());
+            Console.WriteLine(p1.CalculateRoute(p3).Count);
+            
+            //Assert.AreEqual(3, p1.CalculateDistance(p3));
+            //Assert.AreEqual(0, p1.CalculateDistance(p1));
+            
+            Assert.Pass();
+            
         }
     }
 }
