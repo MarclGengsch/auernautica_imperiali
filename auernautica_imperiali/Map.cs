d17c8923e0d869395555508dbb6d1387a38ea84f
Gaduso <g4duso@gmail.com>
2020-11-07 15:50:18 +0100

    7.11.2020 Move Fixed

--- auernautica_imperiali/Map.cs
+++ auernautica_imperiali/Map.cs
@@ -3,42 +3,59 @@ using System.Text;
 using System.Threading;
 
 namespace auernautica_imperiali {
-    public class Map {        //kein Unittest, da dumm
+    public class Map {
+        //kein Unittest, da dumm
         public const int Altitude = 5, Width = 15, Height = 15;
 
-        public Map() {
+        public Map()
+        {
         }
 
         public void PrintMap()
         {
             int height = 1;
             StringBuilder sb = new StringBuilder();
-            for (int i = 1; i <= Height; i++, height++) {
-                sb.Append(height.ToString() + "  ");
-                for (int j = 1; j <= Altitude; j++) {
+            for (int i = 1; i <= Height; i++, height++)
+            {
+                if (height < 10 )
+                    sb.Append(" " + height + "  ");
+                else
+                    sb.Append(height + "  ");
+                for (int j = 1; j <= Altitude; j++)
+                {
                     for (int k = 1; k <= Width; k++)
                     {
                         bool written = false;
-                        foreach (AOrk _orks in GameEngine.OrkList) {
-                            if (_orks.Equals(new Point(k,i,j))) {
+                        foreach (AOrk _orks in GameEngine.OrkList)
+                        {
+                            if (_orks.Equals(new Point(k, i, j)))
+                            {
                                 sb.Append("o ");
                                 written = true;
                             }
                         }
-                        foreach (AImperiali _imperiali in GameEngine.ImperialiList) {
-                            if (_imperiali.Equals(new Point(k, i, j))) {
+
+                        foreach (AImperiali _imperiali in GameEngine.ImperialiList)
+                        {
+                            if (_imperiali.Equals(new Point(k, i, j)))
+                            {
                                 sb.Append("i ");
                                 written = true;
                             }
                         }
-                        if (!written) {
+
+                        if (!written)
+                        {
                             sb.Append("_ ");
                         }
                     }
+
                     sb.Append("  ");
                 }
+
                 sb.AppendLine();
             }
+
             Console.WriteLine(sb.ToString());
         }
     }
--- auernautica_imperiali/Program.cs
+++ auernautica_imperiali/Program.cs
@@ -9,6 +9,7 @@ namespace auernautica_imperiali {
             Logger.LOG_TO_CONSOLE = true;
             AirCraftFactory.GetInstance().MakeAircraft(3,14,1, EAirCraftType.BIGBURNA);
             AirCraftFactory.GetInstance().MakeAircraft(3,1,1, EAirCraftType.BIGBURNA);
+            Console.WriteLine();
             //AirCraftFactory.GetInstance().MakeAircraft(4,14,2, EAirCraftType.BIGBURNA);
             //AirCraftFactory.GetInstance().MakeAircraft(5,14,3, EAirCraftType.BIGBURNA);
             //AirCraftFactory.GetInstance().MakeAircraft(6,14,4, EAirCraftType.BIGBURNA);
