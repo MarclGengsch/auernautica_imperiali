99aebfbe7e036014d37cb12fe5de9fb20507818d
Gaduso <g4duso@gmail.com>
2020-11-07 14:21:24 +0100

    1.11.2020 Latest

--- auernautica_imperiali/Program.cs
+++ auernautica_imperiali/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Channels;
 
 namespace auernautica_imperiali {
     class Program {
         static void Main(string[] args) {
             AirCraftFactory.GetInstance().MakeAircraft(3,14,1, EAirCraftType.BIGBURNA);
-            AirCraftFactory.GetInstance().MakeAircraft(4,14,2, EAirCraftType.BIGBURNA);
-            AirCraftFactory.GetInstance().MakeAircraft(5,14,3, EAirCraftType.BIGBURNA);
-            AirCraftFactory.GetInstance().MakeAircraft(6,14,4, EAirCraftType.BIGBURNA);
-            AirCraftFactory.GetInstance().MakeAircraft(7,14,5, EAirCraftType.BIGBURNA);
-            AirCraftFactory.GetInstance().MakeAircraft(7,1,2, EAirCraftType.BLUEDEVIL);
+            //AirCraftFactory.GetInstance().MakeAircraft(4,14,2, EAirCraftType.BIGBURNA);
+            //AirCraftFactory.GetInstance().MakeAircraft(5,14,3, EAirCraftType.BIGBURNA);
+            //AirCraftFactory.GetInstance().MakeAircraft(6,14,4, EAirCraftType.BIGBURNA);
+            //AirCraftFactory.GetInstance().MakeAircraft(7,14,5, EAirCraftType.BIGBURNA);
+            //AirCraftFactory.GetInstance().MakeAircraft(7,1,2, EAirCraftType.BLUEDEVIL);
+            GameEngine.GetInstance().map.PrintMap();
+            Console.WriteLine(GameEngine.OrkList.Count);
+            GameEngine.GetInstance().ExecuteCommands(new List<ICommand>(){new MoveCommand(GameEngine.OrkList[0], new Point(3, 10, 1))});
+            
             
             GameEngine.GetInstance().map.PrintMap();
             
